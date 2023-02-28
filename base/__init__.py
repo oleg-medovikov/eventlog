f60ce74dac43b724654f2e67827a45a15a73b23b
oleg-medovikov <oleg.medovikov@mail.ru>
2023-02-28 16:50:29 +0300

    add dispetcher

--- base/__init__.py
+++ base/__init__.py
@@ -1,7 +1,8 @@
 from .users import t_users
+from .base import database
 
 
 __all__ = [
+    'database',
     't_users',
-
     ]
---
+++ disp/__init__.py
@@ -0,0 +1,8 @@
+from .dispetcher import bot, dp
+from .on_startup import on_startup
+
+__all__ = [
+    'bot',
+    'dp',
+    'on_startup',
+    ]
